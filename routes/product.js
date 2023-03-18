b1775d9ab60713ebfa4ae8d66dd6f34304123834
unknown <shirleyapril23@gmail.com>
2023-03-18 18:31:28 +0300

    add routes

--- routes/product.js
+++ routes/product.js
@@ -1,1 +1,9 @@
 const express = require("express")
+const { getAllProducts, addProduct } = require("../controllers/productController")
+
+const router = express.Router()
+
+router.get("/add-product", addProduct)
+router.get("/products", getAllProducts)
+
+module.exports = router
