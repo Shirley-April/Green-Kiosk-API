9d265e399e7b68c6619d05cdd50cc8a1b180ad42
unknown <shirleyapril23@gmail.com>
2023-04-11 23:54:39 +0300

    add images

--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,16 @@
-const express = require("express")
-const { getAllProducts, addProduct, deleteProduct, updateProduct } = require("../controllers/productController")
+const express = require("express");
+const {
+  getAllProducts,
+  addProduct,
+  deleteProduct,
+  updateProduct,
+} = require("../controllers/productController");
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/add-product", addProduct)
-router.get("/products", getAllProducts)
-router.delete("/delete/:_id", deleteProduct)
-router.patch("/update/:_id", updateProduct)
+router.post("/add-product", addProduct);
+router.get("/products", getAllProducts);
+router.delete("/delete/:_id", deleteProduct);
+router.patch("/update/:_id", updateProduct);
 
-
-module.exports = router
+module.exports = router;
